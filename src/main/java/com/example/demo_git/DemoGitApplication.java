46d38b34d372c87d50353c92ec4fce4cb5127253
fatimaa28 <kinebocoum74@gmail.com>
2026-02-13 18:34:39 +0000

    Ajout application Spring Boot

--- src/main/java/com/example/demo_git/DemoGitApplication.java
+++ src/main/java/com/example/demo_git/DemoGitApplication.java
@@ -2,6 +2,8 @@ package com.example.demo_git;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoGitApplication {
@@ -10,4 +12,14 @@ public class DemoGitApplication {
 		SpringApplication.run(DemoGitApplication.class, args);
 	}
 
+}
+
+// Classe interne non publique
+@RestController
+class HelloWorldController {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello, World!";
+	}
 }
